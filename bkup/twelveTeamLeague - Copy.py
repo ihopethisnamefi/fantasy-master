e9e255fdbd91179a77058c852c98429fff39b418
ihopethisnamefi <jonthibeault@gmail.com>
2016-12-12 12:17:57 -0600

    Cleanup

---
+++ bkup/twelveTeamLeague - Copy.py
@@ -0,0 +1,93 @@
+# Fill in the form below with info about your league managers
+# 'name' is there name, this will be a lookup key in the DB, so make sure its the same name you use when importing the data
+# 'nickname' is a shortened version of their name that will show up in the power rankings
+# 'picture' is a direct url to the picture you want to represent the manager in the power rankings
+# 'clubHouse' is the url link to the managers team page
+# 'lastYearPosition' is the position in which the player finished last year (ex. "1st") ("N/A" if no results from last year)
+
+managerInfo = [
+      {
+        'name': 'Jon Thibeault',
+        'nickname': 'Tbo',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/jon_zpsoj9pcyl5.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=1&seasonId=2016',
+        'lastYearPosition': '1st'
+      },
+      {
+        'name': 'Samantha Kuhn',
+        'nickname': 'GOAT',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/sam_zpsoqvfshal.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=2&seasonId=2016',
+        'lastYearPosition': '4th'
+      },
+      {
+        'name': 'Michael Nguyen',
+        'nickname': 'Korea',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/mikey_zpsjjf4lslw.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=3&seasonId=2016',
+        'lastYearPosition': '7th'
+      },
+      {
+        'name': 'Jeremy Hsu',
+        'nickname': 'Jeruhhhmeeeeeeee',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/jeremy_zpsdf3nlgmz.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=4&seasonId=2016',
+        'lastYearPosition': '5th'
+      },
+      {
+        'name': 'Luan Nguyen',
+        'nickname': 'Troll',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/luan_zpsdinkbrdq.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=5&seasonId=2016',
+        'lastYearPosition': '9th'
+      },
+      {
+        'name': 'Billy Juch',
+        'nickname': 'chill',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/bill_zpsl1q7ldpp.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=6&seasonId=2016',
+        'lastYearPosition': '3rd'
+      },
+      {
+        'name': 'Hal Peters',
+        'nickname': 'Dirty',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/hal_zpsfbyc1kbk.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=3&seasonId=2016',
+        'lastYearPosition': '10th'
+      },
+	  {
+        'name': 'Jeff Klein',
+        'nickname': 'ffej',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/lum_zpswto7nddh.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=8&seasonId=2012',
+        'lastYearPosition': 'N/A'
+      },
+	  {
+        'name': 'Matt Leal',
+        'nickname': 'Brown #3',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/lum_zpswto7nddh.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=10&seasonId=2012',
+        'lastYearPosition': 'N/A'
+      },
+      {
+        'name': 'Matt Bui',
+        'nickname': 'Booz',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/matt_zpsiuujvts3.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=12&seasonId=2016',
+        'lastYearPosition': '6th'
+      },
+      {
+        'name': 'Vijan Patel',
+        'nickname': 'Brown #2',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/vijan_zpsz2smncci.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=11&seasonId=2015',
+        'lastYearPosition': '11th'
+      },
+	  {
+        'name': 'Elton Wong',
+        'nickname': 'wong',
+        'picture': 'http://i21.photobucket.com/albums/b291/tbonesteakyumm/lum_zpswto7nddh.png',
+        'clubHouse': 'http://games.espn.com/ffl/clubhouse?leagueId=164548&teamId=9&seasonId=2013',
+        'lastYearPosition': 'N/A'
+      }
+]
